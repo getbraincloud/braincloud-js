01099d3fc3f7c66824b3d0f88260f3a6038edbe0
Brad Hill <bradleyh@bitheads.com>
2016-10-12 13:09:30 -0400

    Increment version to 3.1, add new APIs for release

---
+++ src/brainCloudClient-mail.js
@@ -0,0 +1,54 @@
+
+brainCloudClient.mail = {};
+
+brainCloudClient.SERVICE_MAIL = "mail";
+
+brainCloudClient.mail.OPERATION_SEND_BASIC_EMAIL = "SEND_BASIC_EMAIL";
+brainCloudClient.mail.OPERATION_SEND_ADVANCED_EMAIL = "SEND_ADVANCED_EMAIL";
+
+/**
+ * Sends a simple text email to the specified player
+ *
+ * Service Name - mail
+ * Service Operation - SEND_BASIC_EMAIL
+ *
+ * @param profileId The user to send the email to
+ * @param subject The email subject
+ * @param body The email body
+ * @param callback The method to be invoked when the server response is received
+ */
+brainCloudClient.mail.sendBasicEmail = function(profileId, subject, body, callback) {
+    brainCloudManager.sendRequest({
+        service: brainCloudClient.SERVICE_MAIL,
+        operation: brainCloudClient.mail.OPERATION_SEND_BASIC_EMAIL,
+        data: {
+            profileId: profileId,
+            subject: subject,
+            body: body
+        },
+        callback: callback
+    });
+};
+
+/**
+ * Sends an advanced email to the specified player
+ *
+ * Service Name - mail
+ * Service Operation - SEND_ADVANCED_EMAIL
+ *
+ * @param profileId The user to send the email to
+ * @param serviceParams Parameters to send to the email service. See the documentation for
+ *	a full list. http://getbraincloud.com/apidocs/apiref/#capi-mail
+ * @param in_callback The method to be invoked when the server response is received
+ */
+brainCloudClient.mail.sendAdvancedEmail = function(profileId, serviceParams, callback) {
+    brainCloudManager.sendRequest({
+        service: brainCloudClient.SERVICE_MAIL,
+        operation: brainCloudClient.mail.OPERATION_SEND_ADVANCED_EMAIL,
+        data: {
+            profileId: profileId,
+            serviceParams: serviceParams
+        },
+        callback: callback
+    });
+};
