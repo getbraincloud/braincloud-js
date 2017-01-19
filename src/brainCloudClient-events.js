9975924c4dda90d973fa4c2d6f5c543679780f0f
Brad Hill <bradleyh@bitheads.com>
2017-01-19 13:30:48 -0500

    Re-factor the event system

--- src/brainCloudClient-events.js
+++ src/brainCloudClient-events.js
@@ -26,25 +26,13 @@ brainCloudClient.event.OPERATION_GET_EVENTS = "GET_EVENTS";
  * @param toPlayerId The id of the player who is being sent the event
  * @param eventType The user-defined type of the event.
  * @param eventData The user-defined data for this event encoded in JSON.
- * @param recordLocally If true, a copy of this event will be saved in the
- * user's sent events mailbox.
  * @param callback The method to be invoked when the server response is received
- *
- * @return The JSON returned in the callback includes the server generated
- * event id and is as follows:
- * {
- *   "status":200,
- *   "data":{
- *     "eventId":3824
- *   }
- * }
  */
-brainCloudClient.event.sendEvent = function(toPlayerId, eventType, eventData, recordLocally, callback) {
+brainCloudClient.event.sendEvent = function(toPlayerId, eventType, eventData, callback) {
     var message = {
             toId: toPlayerId,
             eventType: eventType,
-            eventData: eventData,
-            recordLocally: recordLocally                
+            eventData: eventData
     };
 
     brainCloudManager.sendRequest({
@@ -61,15 +49,13 @@ brainCloudClient.event.sendEvent = function(toPlayerId, eventType, eventData, re
  * Service Name - Event
  * Service Operation - UpdateEventData
  *
- * @param fromPlayerId The id of the player who sent the event
  * @param eventId The event id
  * @param eventData The user-defined data for this event encoded in JSON.
  * @param callback The method to be invoked when the server response is received
  */
-brainCloudClient.event.updateIncomingEventData = function(fromPlayerId, eventId, eventData, callback) {
+brainCloudClient.event.updateIncomingEventData = function(eventId, eventData, callback) {
     var message = {
-        fromId: fromPlayerId,
-        eventId: eventId,
+        evId: eventId,
         eventData: eventData
     };
     brainCloudManager.sendRequest({
@@ -86,47 +72,19 @@ brainCloudClient.event.updateIncomingEventData = function(fromPlayerId, eventId,
  * Service Name - Event
  * Service Operation - DeleteIncoming
  *
- * @param fromPlayerId The id of the player who sent the event
  * @param eventId The event id
  * @param callback The method to be invoked when the server response is received
  */
-brainCloudClient.event.deleteIncomingEvent = function(fromPlayerId, eventId, callback) {
+brainCloudClient.event.deleteIncomingEvent = function(eventId, callback) {
     brainCloudManager.sendRequest({
         service: brainCloudClient.SERVICE_EVENT,
         operation: brainCloudClient.event.OPERATION_DELETE_INCOMING,
         data: {
-            eventId: eventId,
-            fromId: fromPlayerId
+            evId: eventId
         },
         callback: callback
     });
 };
-
-/**
- * Delete an event from the player's sent mailbox.
- *
- * Note that only events sent with the "recordLocally" flag
- * set to true will be added to a player's sent mailbox.
- *
- * Service Name - Event
- * Service Operation - DeleteSent
- *
- * @param toPlayerId The id of the player who is being sent the even
- * @param eventId The event id
- * @param callback The method to be invoked when the server response is received
- */
-brainCloudClient.event.deleteSentEvent = function(toPlayerId, eventId, callback) {
-    brainCloudManager.sendRequest({
-        service: brainCloudClient.SERVICE_EVENT,
-        operation: brainCloudClient.event.OPERATION_DELETE_SENT,
-        data: {
-            eventId: eventId,
-            toId: toPlayerId
-        },
-        callback: callback
-    });
-};
-
 
 /**
  * Get the events currently queued for the player.
@@ -134,20 +92,13 @@ brainCloudClient.event.deleteSentEvent = function(toPlayerId, eventId, callback)
  * Service Name - Event
  * Service Operation - GetEvents
  *
- * @param includeIncomingEvents Get events sent to the player
- * @param includeSentEvents Get events sent from the player
  * @param callback The method to be invoked when the server response is received
  */
-brainCloudClient.event.getEvents = function(includeIncomingEvents, includeSentEvents, callback) {
-       brainCloudManager.sendRequest({
-           service: brainCloudClient.SERVICE_EVENT,
-           operation: brainCloudClient.event.OPERATION_GET_EVENTS,
-           data: {
-               includeIncomingEvents: includeIncomingEvents,
-               includeSentEvents: includeSentEvents
-           },
-           callback: callback
-       });
-   };
-
-
+brainCloudClient.event.getEvents = function(callback) {
+    brainCloudManager.sendRequest({
+            service: brainCloudClient.SERVICE_EVENT,
+            operation: brainCloudClient.event.OPERATION_GET_EVENTS,
+            data: null,
+            callback: callback
+    });
+};
