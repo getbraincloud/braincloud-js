943798dfd0f4a8380a60657dd279ade1f22a2d59
bit-jonathanm <jonathanm@bitheads.com>
2018-04-17 15:45:13 -0400

    Adding Smart Switch to Wrapper

--- src/brainCloudWrapper.js
+++ src/brainCloudWrapper.js
@@ -382,6 +382,312 @@ function BrainCloudWrapper(wrapperName) {
 			});
 	};
 
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user with a custom Email and Password. Note that the client app
+	 * is responsible for collecting and storing the e-mail and potentially password
+	 * (for convenience) in the client data. For the greatest security,
+	 * force the user to re-enter their password at each login
+	 * (or at least give them that option).
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param email {string} - The e-mail address of the user
+	 * @param password {string} - The password of the user
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateEmailPassword = function (email, password, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateEmailPassword(
+				email,
+				password,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user via cloud code (which in turn validates the supplied credentials against an external system).
+	 * This allows the developer to extend brainCloud authentication to support other backend authentication systems.
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param userId {string} - The userId
+	 * @param token {string} - The user token (password etc)
+	 * @param externalAuthName {string} - The name of the cloud script to call for external authentication
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateExternal = function (userId, token, externalAuthName, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateExternal(
+				userId,
+				token,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user with brainCloud using their Facebook Credentials
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param facebookId {string} - The Facebook id of the user
+	 * @param facebookToken {string} - The validated token from the Facebook SDK
+	 * (that will be further validated when sent to the bC service)
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateFacebook = function (facebookId, facebookToken, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateFacebook(
+				facebookId,
+				facebookToken,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user using their Game Center id
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param gameCenterId {string} - The player's game center id
+	 (use the playerID property from the local GKPlayer object)
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateGameCenter = function (gameCenterId, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateGameCenter(
+				gameCenterId,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user using a google user id (email address) and google authentication token.
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param googleId {string} - String representation of google+ userid (email)
+	 * @param googleToken {string} - The authentication token derived via the google apis.
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * If set to false, you need to handle errors in the case of new users.
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateGoogle = function (googleId, googleToken, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateGoogle(
+				googleId,
+				googleToken,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user using a steam userId and session ticket (without any validation on the userId).
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param userid  String representation of 64 bit steam id
+	 * @param sessionticket  The session ticket of the user (hex encoded)
+	 * @param forceCreate Should a new profile be created for this user if the account does not exist?
+	 * @param callback The method to be invoked when the server response is received
+	 *
+	 * @returns   performs the in_success callback on success, in_failure callback on failure
+	 *
+	 */
+	bcw.smartSwitchAuthenticateSteam = function (userId, sessionTicket, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateSteam(
+				userId,
+				sessionTicket,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Authenticate the user using a Twitter user ID, authentication token, and secret from Twitter
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param userId {string} - String representation of Twitter user ID
+	 * @param token {string} - The authentication token derived via the Twitter APIs
+	 * @param secret {string} - The secret given when attempting to link with Twitter
+	 * @param forceCreate {boolean} - Should a new profile be created for this user if the account does not exist?
+	 * If set to false, you need to handle errors in the case of new users.
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateTwitter = function (userId, token, secret, forceCreate, responseHandler)
+	{
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateTwitter(
+				userId,
+				token,
+				secret,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	/**
+	 * Smart Switch Authenticate will logout of the current profile, and switch to the new authentication type.
+	 * In event the current session was previously an anonymous account, the smart switch will delete that profile.
+	 * Use this function to keep a clean designflow from anonymous to signed profiles
+	 *
+	 * Method authenticates the user using universal credentials
+	 *
+	 * Service Name - authenticationV2
+	 * Service Operation - AUTHENTICATE
+	 *
+	 * @param userId {string} - The user's id. Can be any string you want.
+	 * @param userPassword {string} - The user's password. Can be any string you want.
+	 * @param forceCreate {boolean} - True if we force creation of the user if they do not already exist.
+	 * If set to false, you need to handle errors in the case of new users.
+	 * @param responseHandler {function} - The user callback method
+	 */
+	bcw.smartSwitchAuthenticateUniversal = function(userId, userPassword, forceCreate, responseHandler) {
+
+		bcw._initializeIdentity(true);
+
+		authenticationCallback = function() {
+			bcw.brainCloudClient.authentication.authenticateUniversal(
+				userId,
+				userPassword,
+				forceCreate,
+				function(result) {
+					bcw._authResponseHandler(result);
+					responseHandler(result);
+				});
+		};
+
+		bcw.brainCloudClient.identity.getIdentities(getIdentitiesCallback(authenticationCallback));
+	};
+
+	getIdentitiesCallback = function(callback) {
+		identitiesCallback = function (response)
+		{
+			try {
+				let identities = JSON.stringify(response.data.identities);
+				let len = identities.length;
+
+				if(identities === "{}" || identities === "") {
+					bcw.brainCloudClient.playerState.deleteUser(callback);
+				} else {
+					bcw.brainCloudClient.playerState.logout(callback);
+				}
+			} catch(e) {
+				bcw.brainCloudClient.playerState.logout(callback);
+			}
+		}
+
+		return identitiesCallback;
+	};
+
 	/** Method authenticates the user using universal credentials
 	 *
 	 * @param responseHandler {function} - The user callback method
