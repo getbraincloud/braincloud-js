8f46a697d33fd5a697ae16a2c6b61f3d551004b6
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2018-09-21 16:01:24 -0400

    Updating

--- src/brainCloudClient-virtual-currency.js
+++ src/brainCloudClient-virtual-currency.js
@@ -10,6 +10,9 @@ function BCVirtualCurrency() {
     bc.virtualCurrency.OPERATION_GET_PARENT_CURRENCY = "GET_PARENT_VC";
     bc.virtualCurrency.OPERATION_GET_PEER_CURRENCY = "GET_PEER_VC";
 
+    bc.virtualCurrency.OPERATION_AWARD_VC = "AWARD_VC";
+    bc.virtualCurrency.OPERATION_CONSUME_PLAYER_VC = "CONSUME_VC";
+
     /**
      * Retrieve the user's currency account. Optional parameters: vcId (if retrieving all currencies).
      *
@@ -91,17 +94,17 @@ function BCVirtualCurrency() {
      *  @deprecated For security reasons calling this API from the client is not recommended, and is rejected at the server by default. To over-ride, enable the 'Allow Currency Calls from Client' compatibility setting in the Design Portal.
      *
      * @param vcId
-     * @param amount
+     * @param vcAmount
      * @param callback The method to be invoked when the server response is received
      */
-    bc.virtualCurrency.awardCurrency = function(vcId, amount, callback) {
+    bc.virtualCurrency.awardCurrency = function(vcId, vcAmount, callback) {
         var message = {
             vcId: vcId,
-            vcAmount: amount
+            vcAmount: vcAmount
         };
         bc.brainCloudManager.sendRequest({
             service: bc.SERVICE_VIRTUAL_CURRENCY,
-            operation: bc.product.OPERATION_AWARD_VC,
+            operation: bc.virtualCurrency.OPERATION_AWARD_VC,
             data: message,
             callback: callback
         });
@@ -118,16 +121,16 @@ function BCVirtualCurrency() {
      * @deprecated For security reasons calling this API from the client is not recommended, and is rejected at the server by default. To over-ride, enable the 'Allow Currency Calls from Client' compatibility setting in the Design Portal.
      *
      * @param vcId
-     * @param peerCode
+     * @param vcAmount
      * @param callback The method to be invoked when the server response is received
      */
-    bc.virtualCurrency.consumeCurrency = function(vcId, amount, callback) {
+    bc.virtualCurrency.consumeCurrency = function(vcId, vcAmount, callback) {
         bc.brainCloudManager.sendRequest({
             service: bc.SERVICE_VIRTUAL_CURRENCY,
-            operation: bc.product.OPERATION_CONSUME_PLAYER_VC,
+            operation: bc.virtualCurrency.OPERATION_CONSUME_PLAYER_VC,
             data: {
                 vcId: vcId,
-                vcAmount: amount
+                vcAmount: vcAmount
             },
             callback: callback
         });
