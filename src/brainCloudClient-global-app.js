e1a7ff3eaefbc11c99a96e43e4c1e641a5fef8ce
Daivuk <daivuk@gmail.com>
2021-09-27 15:35:07 -0400

    BCLOUD-1079 getSelectedProperties/InCategories

--- src/brainCloudClient-global-app.js
+++ src/brainCloudClient-global-app.js
@@ -6,6 +6,8 @@ function BCGlobalApp() {
 
 	bc.SERVICE_GLOBAL_APP = "globalApp";
 	bc.globalApp.OPERATION_READ_PROPERTIES = "READ_PROPERTIES";
+	bc.globalApp.OPERATION_READ_SELECTED_PROPERTIES = "READ_SELECTED_PROPERTIES";
+	bc.globalApp.OPERATION_READ_PROPERTIES_IN_CATEGORIES = "READ_PROPERTIES_IN_CATEGORIES";
 
 	/**
 	 * Read game's global properties
@@ -23,6 +25,51 @@ function BCGlobalApp() {
 		});
 	};
 
+	/**
+	 * Returns a list of properties, identified by the property names provided.
+	 * If a property from the list isn't found, it just isn't returned (no error).
+	 *
+	 * Service Name - GlobalApp
+	 * Service Operation - READ_SELECTED_PROPERTIES
+	 * 
+	 * @param propertyNames Specifies which properties to return
+	 * @param callback The method to be invoked when the server response is received
+	 */
+	bc.globalApp.readSelectedProperties = function(propertyNames, callback) {
+        var message = {
+            propertyNames: propertyNames
+        };
+
+		bc.brainCloudManager.sendRequest({
+			service: bc.SERVICE_GLOBAL_APP,
+			operation: bc.globalApp.OPERATION_READ_SELECTED_PROPERTIES,
+			data: message,
+			callback: callback
+		});
+	};
+
+	/**
+	 * Returns a list of properties, identified by the categories provided.
+	 * If a category from the list isn't found, it just isn't returned (no error).
+	 *
+	 * Service Name - GlobalApp
+	 * Service Operation - READ_PROPERTIES_IN_CATEGORIES
+	 * 
+	 * @param categories Specifies which category to return
+	 * @param in_callback The method to be invoked when the server response is received
+	 */
+	bc.globalApp.readPropertiesInCategories = function(categories, callback) {
+        var message = {
+            categories: categories
+        };
+
+		bc.brainCloudManager.sendRequest({
+			service: bc.SERVICE_GLOBAL_APP,
+			operation: bc.globalApp.OPERATION_READ_PROPERTIES_IN_CATEGORIES,
+			data: message,
+			callback: callback
+		});
+	};
 }
 
 BCGlobalApp.apply(window.brainCloudClient = window.brainCloudClient || {});
