2783138d306b62e94705d23200441374c305787e
Daivuk <daivuk@gmail.com>
2021-11-08 07:32:36 -0500

    Fixed binaryMessage for k6

--- src/brainCloudRttComms.js
+++ src/brainCloudRttComms.js
@@ -13,6 +13,37 @@ var disconnectMessage= null;
 //> ADD IF K6
 //+ import ws from 'k6/ws';
 //+ import { check } from "k6";
+//+ function Utf8ArrayToStr(array) {
+//+     var out, i, len, c;
+//+     var char2, char3;
+//+     out = "";
+//+     len = array.length;
+//+     i = 0;
+//+     while(i < len) {
+//+     c = array[i++];
+//+     switch(c >> 4)
+//+     { 
+//+       case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+//+         // 0xxxxxxx
+//+         out += String.fromCharCode(c);
+//+         break;
+//+       case 12: case 13:
+//+         // 110x xxxx   10xx xxxx
+//+         char2 = array[i++];
+//+         out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+//+         break;
+//+       case 14:
+//+         // 1110 xxxx  10xx xxxx  10xx xxxx
+//+         char2 = array[i++];
+//+         char3 = array[i++];
+//+         out += String.fromCharCode(((c & 0x0F) << 12) |
+//+                        ((char2 & 0x3F) << 6) |
+//+                        ((char3 & 0x3F) << 0));
+//+         break;
+//+     }
+//+     }
+//+     return out;
+//+ }
 //> END
 
 function getBrowserName() {
@@ -109,6 +140,11 @@ function BrainCloudRttComms (m_client) {
 //+         socket.on('close', bcrtt.onSocketClose);
 //+         socket.on('open', bcrtt.onSocketOpen);
 //+         socket.on('message', bcrtt.onSocketMessage);
+//+         socket.on('binaryMessage', msg =>
+//+         {
+//+             var message = Utf8ArrayToStr(new Uint8Array(msg))
+//+             bcrtt.onSocketMessage(message);
+//+         });
 //+     });
 //> END
 //> REMOVE IF K6
