15b594226d928575278d39e75823cbaa0df4dc9a
bitheadCody <codym@bitheads.com>
2025-06-10 10:39:41 -0400

    created test for empty notification token

--- src/brainCloudClient-push-notifications.js
+++ src/brainCloudClient-push-notifications.js
@@ -66,13 +66,13 @@ function BCPushNotifications() {
      * @param callback The method to be invoked when the server response is received
      */
     bc.pushNotification.registerPushNotificationDeviceToken = function(deviceType, deviceToken, callback) {
-        const STATUS_CODE = 400;
+        var STATUS_CODE = 400;
 
         if (!deviceToken || deviceToken.trim().length === 0) {
-            const errorJson = JSON.stringify({
+            var errorJson = JSON.stringify({
                 status: STATUS_CODE,
                 reason_code: bc.INVALID_DEVICE_TOKEN,
-                message: `Invalid device token: ${deviceToken}`
+                message: "Invalid device token: " + deviceToken
             });
             
             // fire off the error if its there
